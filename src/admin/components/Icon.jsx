55a5e6ab6b5cee1a810cefb3bc8ce68575ada4ad
Bob_Smith <reversetype@gmail.com>
2023-09-12 16:44:51 -0400

    📦 chore(Icon.jsx, Logo.jsx): add Icon and Logo components The Icon.jsx and Logo.jsx files have been added to the src/admin/components directory. These components use the useTheme hook from the Payload admin utilities to determine the current theme and display the appropriate logo or icon based on the theme. The Icon component displays an image of the Appily logo, while the Logo component displays the full Appily logo.

--- src/admin/components/Icon.jsx
+++ src/admin/components/Icon.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
 import { useTheme } from 'payload/dist/admin/components/utilities/Theme'
 
 import IconDarkSrc from './assets/appily-icon-dark.svg'
 import IconLightSrc from './assets/appily-icon-light.svg'
 
-interface Props {
-  theme: string
-}
-
-const Icon: React.FC<Props> = () => {
+const Icon = () => {
   const { theme } = useTheme()
   const logoSrc = theme === 'light' ? IconLightSrc : IconDarkSrc
   return <img src={logoSrc} alt="Appily logo" />
--- src/admin/components/Logo.jsx
+++ src/admin/components/Logo.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
 import { useTheme } from 'payload/dist/admin/components/utilities/Theme'
 
 import LogoDarkSrc from './assets/appily-logo-dark.svg'
 import LogoLightSrc from './assets/appily-logo-light.svg'
 
-interface Props {
-  theme: string
-}
-
-const Logo: React.FC<Props> = () => {
+const Logo = () => {
   const { theme } = useTheme()
   const logoSrc = theme === 'light' ? LogoLightSrc : LogoDarkSrc
   return <img src={logoSrc} alt="Appily logo" />
