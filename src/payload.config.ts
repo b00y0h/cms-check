cfeb1939036d43238bf26528e986a6f455a05947
Andrei Olea <andrei@andreiolea.com>
2024-05-10 16:54:56 -0400

    ✨ feat(admin): add custom styles to change font weight of headings in Rich Text Editor
    🔧 refactor(payload.config.ts): fix quotes inconsistency and add missing css path
    🚀 chore(payload.config.ts): update admin configuration with custom CSS path

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,112 +1,112 @@
-import { payloadCloud } from '@payloadcms/plugin-cloud'
-import FormBuilder from '@payloadcms/plugin-form-builder'
-import nestedDocs from '@payloadcms/plugin-nested-docs'
-import seo from '@payloadcms/plugin-seo'
-import type { GenerateTitle } from '@payloadcms/plugin-seo/types'
-import path from 'path'
-import { buildConfig } from 'payload/config'
+import { payloadCloud } from "@payloadcms/plugin-cloud";
+import FormBuilder from "@payloadcms/plugin-form-builder";
+import nestedDocs from "@payloadcms/plugin-nested-docs";
+import seo from "@payloadcms/plugin-seo";
+import type { GenerateTitle } from "@payloadcms/plugin-seo/types";
+import path from "path";
+import { buildConfig } from "payload/config";
 
-import { CarouselCards } from './collections/CarouselCards'
-import { LeadTypes } from './collections/LeadTypes'
-import { Media } from './collections/Media'
-import { Pages } from './collections/Pages'
-import { Partners } from './collections/Partners'
-import { Posts } from './collections/Posts'
-import Users from './collections/Users'
-import NoIndex from './components/custom/Noindex'
+import { CarouselCards } from "./collections/CarouselCards";
+import { LeadTypes } from "./collections/LeadTypes";
+import { Media } from "./collections/Media";
+import { Pages } from "./collections/Pages";
+import { Partners } from "./collections/Partners";
+import { Posts } from "./collections/Posts";
+import Users from "./collections/Users";
+import NoIndex from "./components/custom/Noindex";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
-import { Header } from './globals/Header'
-import { Footer } from './globals/Footer'
-
+import { Header } from "./globals/Header";
+import { Footer } from "./globals/Footer";
 
 const generateTitle: GenerateTitle = () => {
-  return 'Appliy CMS'
-}
+	return "Appliy CMS";
+};
 
-const mockModulePath = path.resolve(__dirname, './emptyModuleMock.js')
+const mockModulePath = path.resolve(__dirname, "./emptyModuleMock.js");
 
 export default buildConfig({
-  admin: {
-    livePreview: {
-      url:process.env.PAYLOAD_PUBLIC_SITE_URL,
-      breakpoints: [
-        {
-          label: 'Mobile',
-          name: 'mobile',
-          width: 375,
-          height: 667,
-        },
-      ],
-    },
-    bundler: webpackBundler(),
-    meta: {
-      titleSuffix: '- Appily',
-      favicon: '/assets/favicon.svg',
-      ogImage: '/assets/logo.svg',
-    },
-    user: Users.slug,
-    components: {
-      // graphics: {
-      //   Logo,
-      //   Icon,
-      // },
-      // The BeforeDashboard component renders the 'welcome' block that you see after logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import BeforeDashboard statement on line 15.
-      // beforeDashboard: [BeforeDashboard],
-      beforeLogin:[NoIndex]
-    },
-    webpack: config => ({
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve?.alias,
-          express: mockModulePath,
-        },
-      },
-    }),
-    
-  },
-  editor: lexicalEditor({}),
-  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  collections: [Users, Pages, Posts, Media, Partners, CarouselCards, LeadTypes],
-  globals: [Header, Footer],
-  typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
-  },
-  graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
-  },
-  db: mongooseAdapter({
-    url: process.env.MONGODB_URI
-  }),
-  ...(process.env.PAYLOAD_PUBLIC_SITE_URL
-    ? {
-        cors: [process.env.PAYLOAD_PUBLIC_SITE_URL].filter(Boolean),
-        csrf: [process.env.PAYLOAD_PUBLIC_SITE_URL].filter(Boolean),
-      }
-    : {}),
-  plugins: [
-    FormBuilder({
-      fields: {
-        payment: false,
-      },
-    }),
-    nestedDocs({
-      collections: ['pages'],
-      generateLabel: (_, doc) => doc.title as string,
-      generateURL: docs => docs.reduce((url, doc) => `${url}/${doc.slug}`, ''),
-    }),
-    // redirects({
-    //   collections: ['pages'],
-    // }),
-    seo({
-      collections: ['pages'],
-      generateTitle,
-      uploadsCollection: 'media',
-    }),
-    payloadCloud(),
-  ],
-})
+	admin: {
+		livePreview: {
+			url: process.env.PAYLOAD_PUBLIC_SITE_URL,
+			breakpoints: [
+				{
+					label: "Mobile",
+					name: "mobile",
+					width: 375,
+					height: 667,
+				},
+			],
+		},
+		bundler: webpackBundler(),
+		meta: {
+			titleSuffix: "- Appily",
+			favicon: "/assets/favicon.svg",
+			ogImage: "/assets/logo.svg",
+		},
+		user: Users.slug,
+		components: {
+			// graphics: {
+			//   Logo,
+			//   Icon,
+			// },
+			// The BeforeDashboard component renders the 'welcome' block that you see after logging into your admin panel.
+			// Feel free to delete this at any time. Simply remove the line below and the import BeforeDashboard statement on line 15.
+			// beforeDashboard: [BeforeDashboard],
+			beforeLogin: [NoIndex],
+		},
+		webpack: (config) => ({
+			...config,
+			resolve: {
+				...config.resolve,
+				alias: {
+					...config.resolve?.alias,
+					express: mockModulePath,
+				},
+			},
+		}),
+		css: path.resolve(__dirname, "admin/customStyles.scss"),
+	},
+	editor: lexicalEditor({}),
+	serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+	collections: [Users, Pages, Posts, Media, Partners, CarouselCards, LeadTypes],
+	globals: [Header, Footer],
+	typescript: {
+		outputFile: path.resolve(__dirname, "payload-types.ts"),
+	},
+	graphQL: {
+		schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+	},
+	db: mongooseAdapter({
+		url: process.env.MONGODB_URI,
+	}),
+	...(process.env.PAYLOAD_PUBLIC_SITE_URL
+		? {
+				cors: [process.env.PAYLOAD_PUBLIC_SITE_URL].filter(Boolean),
+				csrf: [process.env.PAYLOAD_PUBLIC_SITE_URL].filter(Boolean),
+		  }
+		: {}),
+	plugins: [
+		FormBuilder({
+			fields: {
+				payment: false,
+			},
+		}),
+		nestedDocs({
+			collections: ["pages"],
+			generateLabel: (_, doc) => doc.title as string,
+			generateURL: (docs) =>
+				docs.reduce((url, doc) => `${url}/${doc.slug}`, ""),
+		}),
+		// redirects({
+		//   collections: ['pages'],
+		// }),
+		seo({
+			collections: ["pages"],
+			generateTitle,
+			uploadsCollection: "media",
+		}),
+		payloadCloud(),
+	],
+});
