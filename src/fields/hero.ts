e1466da06eb22df3971213dd889a050e80780ee2
Andrei Olea <andrei@andreiolea.com>
2024-05-20 16:15:21 -0400

    🔄 refactor(hero.ts): reformat code for consistency and readability

--- src/fields/hero.ts
+++ src/fields/hero.ts
@@ -1,77 +1,76 @@
-import type { Field } from 'payload/types'
+import type { Field } from "payload/types";
 
-import linkGroup from './linkGroup'
-
+import linkGroup from "./linkGroup";
 
 export const hero: Field = {
-  name: 'hero',
-  label: false,
-  type: 'group',
-  fields: [
-    {
-      type: 'select',
-      name: 'type',
-      label: 'Type',
-      required: true,
-      defaultValue: 'landingPage',
-      options: [
-        {
-          label: 'None',
-          value: 'none',
-        },
-        {
-          label: 'Landing Page',
-          value: 'landingPage',
-        },
-        {
-          label: 'Result Page',
-          value: 'resultPage',
-        }
-      ],
-    },
-    {
-      name: 'richText',
-      label: 'Description',
-      type: 'richText'
-    },
-    // richText({
-    //   admin: {
-    //     elements: ['h1', largeBody, label, 'link'],
-    //     leaves: ['bold', 'italic', 'underline'],
-    //   },
-    //   required: false
-    // }),
-    linkGroup({
-      overrides: {
-        maxRows: 2,
-      },
-    }),
-    {
-      name: 'title',
-      label: 'Pre Title',
-      type: 'text',
-      admin: {
-        condition: (_, { type } = {}) => ['resultPage'].includes(type),
-      },
-    },
-    {
-      name: 'media',
-      type: 'upload',
-      relationTo: 'media',
-      required: true,
-      admin: {
-        condition: (_, { type } = {}) => ['landingPage'].includes(type),
-      },
-    },
-    {
-      name: 'animation',
-      label: 'Lottie Animation JSON',
-      type: 'upload',
-      required: true,
-      relationTo: 'media',
-      admin: {
-        condition: (_, { type } = {}) => ['resultPage'].includes(type),
-      },
-    },
-  ],
-}
+	name: "hero",
+	label: false,
+	type: "group",
+	fields: [
+		{
+			type: "select",
+			name: "type",
+			label: "Type",
+			required: true,
+			defaultValue: "landingPage",
+			options: [
+				{
+					label: "None",
+					value: "none",
+				},
+				{
+					label: "Landing Page",
+					value: "landingPage",
+				},
+				{
+					label: "Result Page",
+					value: "resultPage",
+				},
+			],
+		},
+		{
+			name: "title",
+			label: "Pre Title",
+			type: "text",
+			admin: {
+				condition: (_, { type } = {}) => ["resultPage"].includes(type),
+			},
+		},
+		{
+			name: "richText",
+			label: "Hero content, includes the H1 title and a brief introduction. ",
+			type: "richText",
+		},
+		// richText({
+		//   admin: {
+		//     elements: ['h1', largeBody, label, 'link'],
+		//     leaves: ['bold', 'italic', 'underline'],
+		//   },
+		//   required: false
+		// }),
+		linkGroup({
+			overrides: {
+				maxRows: 2,
+			},
+		}),
+		{
+			name: "media",
+			type: "upload",
+			relationTo: "media",
+			required: true,
+			admin: {
+				condition: (_, { type } = {}) => ["landingPage"].includes(type),
+			},
+		},
+		{
+			name: "animation",
+			label: "Lottie Animation JSON",
+			type: "upload",
+			required: true,
+			relationTo: "media",
+			admin: {
+				condition: (_, { type } = {}) => ["resultPage"].includes(type),
+			},
+		},
+	],
+};
